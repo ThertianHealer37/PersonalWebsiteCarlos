435b3f91ee7fd6d87152543d2aa83e5f7827f612
unknown <marquezcarlos933@gmail.com>
2022-01-30 14:41:21 -0500

    starting

---
+++ Blog/scroll.js
@@ -0,0 +1,25 @@
+/*window.addEventListener('DOMContentLoaded', () => {
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const id = entry.target.getAttribute('id');
+        if (entry.intersectionRatio > 0) {
+          document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+        } else {
+          document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+        }
+      });
+    });
+  
+    // Track all sections that have an `id` applied
+    document.querySelectorAll('section[id]').forEach((section) => {
+      observer.observe(section);
+    });
+    
+  });
+  */
+
+//What I want to do, once you click to the section keep the text darker, when you click the next one forget and go next
+
+
+tracker = document.querySelector("nav")
---
+++ MainPage/main.js
@@ -0,0 +1,42 @@
+/*
+document.getElementById("ContactInfo")
+document.getElementById("WebsiteTitle")
+
+
+//Declaration of variable with the "let" , when assining it automatically detects the type. 
+let Daniel = crazy;
+
+//Let and const can be used
+/*
+    variables in javascrypt
+        string 
+        number
+        boolean
+        array
+        object
+
+    Conditionals
+    
+        alert : like a print statement.
+    
+        alert()
+    javascript is stairs to the sky without rails
+
+    Methods/Functions
+
+        function name(parameters){
+
+            things;
+            retrun;
+        }
+    click method
+
+            .querySelector('html') is calling for the specific tag in here we are using the <html></html> tag
+        document.querySelector('html').onclick = function(){
+
+            alert('Ouch! Stop poking me');
+
+        }
+
+
+*/
